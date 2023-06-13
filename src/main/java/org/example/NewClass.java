b6a192b2572a67a8a67ae3bcfc8bdf019dc21b8f
Sardorbek Murodov <smurodov870@gmail.com>
2023-06-13 11:17:46 +0500

    this is created at 11.17

---
+++ src/main/java/org/example/NewClass.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class NewClass {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        Scanner scanner = new Scanner(System.in);
+//        System
+    }
+
+}
